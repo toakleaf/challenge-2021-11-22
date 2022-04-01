899a9b1eb791c7ad97558bf950a25ff92396cf98
Tyler Oakleaf <tyler@superpedestrian.com>
2022-04-01 12:18:16 -0400

    quick change

--- src/api/librariesIo.js
+++ src/api/librariesIo.js
@@ -1,27 +1,15 @@
-// out of time to keep debugging why this axios import isn't working
-//
-// import axios from "axios";
+out of time to keep debugging why this axios import isn't working
 
-// const librariesIoAxios = axios.create({
-//   baseURL: process.env.REACT_APP_LIBRARIES_URL,
-//   params: { api_key: process.env.REACT_APP_LIBRARIES_API_KEY },
-// });
+import axios from "axios";
 
-// const librariesIo = {
-//   getProject: async (platform, name) => {
-//     const res = await librariesIoAxios.get(`${platform}/${name}`);
-//     console.log(res);
-//   },
-// };
-
-const baseURL = process.env.REACT_APP_LIBRARIES_URL;
-const key = process.env.REACT_APP_LIBRARIES_API_KEY;
+const librariesIoAxios = axios.create({
+  baseURL: process.env.REACT_APP_LIBRARIES_URL,
+  params: { api_key: process.env.REACT_APP_LIBRARIES_API_KEY },
+});
 
 const librariesIo = {
   getProject: async (platform, name) => {
-    const res = await fetch(`${baseURL}/${platform}/${name}?api_key=${key}`);
-    return await res.json();
+    const res = await librariesIoAxios.get(`${platform}/${name}`);
+    console.log(res);
   },
 };
-
-export default librariesIo;
